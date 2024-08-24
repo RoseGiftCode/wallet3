3ca48ad0cf627175a02d2539c85223d26fc222a2
RoseGiftCode <aigiftson@protonmail.com>
2024-08-24 12:46:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,7 +89,7 @@ const wagmiConfig = createConfig({
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const [web3wallet, setWeb3Wallet] = useState<Web3Wallet | null>(null); // Correct usage here
+  const [web3wallet, setWeb3Wallet] = useState<typeof Web3Wallet | null>(null); // Correct usage here
   const [signClient, setSignClient] = useState<SignClient | null>(null);
   const isMounted = useIsMounted();
 
