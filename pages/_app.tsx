e0a2fae9cac98ad2758fde30cbe0e584f707b359
RoseGiftCode <aigiftson@protonmail.com>
2024-08-24 10:31:43 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ import {
 } from '@rainbow-me/rainbowkit/wallets';
 
 // Define WalletConnect projectId
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'default_project_id_placeholder';
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'dce4c19a5efd3cba4116b12d4fc3689a';
 
 // Define connectors
 const connectors = connectorsForWallets([
@@ -70,7 +70,7 @@ const wagmiConfig = createConfig({
     };
 
     return createClient({
-      transport: http(transportURLs[chain.id] || 'https://default-rpc-url.com'), // Use a fallback URL
+      transport: http(transportURLs[chain.id] || 'https://eth-mainnet.g.alchemy.com/v2/iUoZdhhu265uyKgw-V6FojhyO80OKfmV'), // Use a fallback URL
     });
   },
   chains,
